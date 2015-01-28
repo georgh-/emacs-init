2788cf5d1366458e9df572f80d855041193420dd
georgh <ostankino.tower@gmail.com>
2015-01-27 23:17:45 -0200

    Do not set color theme at work, remove custom theme
    Remove custom theme from customization

--- init.el
+++ init.el
@@ -277,7 +277,6 @@ Uses `copy-region-as-kill'."
  '(browse-kill-ring-separator-face (quote message-header-xheader))
  '(column-number-mode t)
  '(cua-enable-cua-keys nil)
- '(custom-enabled-themes (quote (deeper-blue)))
  '(delete-selection-mode t)
  '(fill-column 79)
  '(fringe-mode (quote (4 . 4)) nil (fringe))
