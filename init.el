cd5a6d5a5937bd3699a92ea5621b47b080f2957c
georgh <georgh@atropos>
2018-01-03 12:49:08 +1000

    Synch package config

--- init.el
+++ init.el
@@ -415,7 +415,7 @@ Output: 123765 or 125816 or 126953"
      ("melpa" . "http://melpa.org/packages/"))))
  '(package-selected-packages
    (quote
-    (diminish counsel use-package smartscan scss-mode powershell php-mode paredit page-break-lines multiple-cursors magit lineno js2-mode guide-key goto-chg expand-region discover browse-kill-ring apropospriate-theme adaptive-wrap)))
+    (beginend web-mode diminish counsel use-package smartscan scss-mode powershell php-mode paredit page-break-lines multiple-cursors magit lineno js2-mode guide-key goto-chg expand-region discover browse-kill-ring apropospriate-theme adaptive-wrap)))
  '(savehist-mode t)
  '(scroll-conservatively 2)
  '(scroll-margin 2)
