031d8dbd03efde28d45ca2b4434bdba6017e1cb6
Nadal <jordi.nadal@atos.net>
2019-04-26 14:08:50 +0800

    USEFUL Use a hydra to navigate through flycheck errors

--- init.el
+++ init.el
@@ -192,7 +192,32 @@ Emacs' kill ring is unmodified after running this function."
   (use-package scss-mode :defer t)
 
   ;; Keybindings with one key, easy to define. Req. by counsel
-  (use-package hydra)
+  (use-package hydra
+    :bind ("C-c f" . hydra-flycheck/body)
+    :config
+    
+    (defhydra hydra-flycheck (:color blue)
+      "
+  ^
+  ^Flycheck^          ^Errors^            ^Checker^
+  ^────────^──────────^──────^────────────^───────^─────
+  _q_ quit            _<_ previous        _?_ describe
+  _M_ manual          _>_ next            _d_ disable
+  _v_ verify setup    _f_ check           _m_ mode
+  ^^                  _l_ list            _s_ select
+  ^^                  ^^                  ^^
+  "
+      ("q" nil)
+      ("<" flycheck-previous-error)
+      (">" flycheck-next-error)
+      ("?" flycheck-describe-checker)
+      ("M" flycheck-manual)
+      ("d" flycheck-disable-checker)
+      ("f" flycheck-buffer)
+      ("l" flycheck-list-errors)
+      ("m" flycheck-mode)
+      ("s" flycheck-select-checker)
+      ("v" flycheck-verify-setup)))
 
   ;; Provides history order to counsel-M-x
   (use-package smex)
